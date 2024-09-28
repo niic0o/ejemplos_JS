832bcd2df84bdf7cb2d216f72b3cb2e8d1d1a589
niic0o <nicosena10@gmail.com>
2024-09-28 16:06:22 -0300

    asignaciones scope y conversiones

--- clase_01_video/ejemplo_01_objeto.js
+++ clase_01_video/ejemplo_01_objeto.js
@@ -1,12 +1,4 @@
-/* console log y explicitacion de un objeto
 console.log("hola mundo!");
-
-const persona = {nombre: "Juan", edad: "40", direccion: "Alla nio"};
-console.log(persona);
-console.table(persona);
-console.warn("esta es una advertencia");
-console.error("este es un error");
-*/
 
 //inspeccion de objetos. Busco a pedro, cambio por Jorge guardo y entro en un if
 const persona = { nombre: "Pedro", edad: "40", direccion: "Alla nio" };
@@ -18,10 +10,10 @@ if (persona.nombre === "Jorge") {
   console.error("esto es un error");
 }
 
-//medir tiempos de ejecucion
-console.time("Tiempo de ejecucion");
+//en cuantos milisegundos mi maquina ejecuta una porcion de codigo?
+console.time("Ejecute este codigo en");
 for (let i= 0; i < 10000; i++){
     // simular un proceso de ejecucion
     let temp = Math.sqrt(i);
 }
-console.timeEnd("Tiempo de ejecucion");
+console.timeEnd("Ejecute este codigo en");
--- clase_01_video/ejemplo_06_asignaciones.js
+++ clase_01_video/ejemplo_06_asignaciones.js
@@ -1,3 +1,4 @@
+// Consejo: ejecutar la extension quokka para ver la magia.
 let x = 8;
 
 x += 12;
@@ -6,4 +7,22 @@ console.log(x);
 x *= 10;
 console.log(x);
 
-x 
+//conversion implicita
+x = '100'
+let a = Number(x) + Number(x);
+console.log(a);
+console.log(typeof(a));
+
+//conversion explicita
+let str = x + ' ' + a;
+console.log(str);
+console.log(typeof(str));
+
+str = null;
+console.log(str);
+
+let tiene_algo = Boolean(str);
+console.log("Tiene algo str?: " + tiene_algo);
+
+tiene_algo = Boolean(a);
+console.log("Tiene algo a?: " + tiene_algo);
